b3f121993582e34a7ef1c9b24515af9993373cdc
叶涛 <821023977@qq.com>
2017-03-14 14:17:53 +0800

    V1
    V1

---
+++ RedisOutputCacheProvider/IOutputCacheConnection.cs
@@ -0,0 +1,17 @@
+﻿//
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+//
+
+using System;
+
+namespace Microsoft.Web.Redis
+{
+    internal interface IOutputCacheConnection
+    {
+        void Set(string key, object entry, DateTime utcExpiry);        
+        object Add(string key, object entry, DateTime utcExpiry);
+        object Get(string key);
+        void Remove(string key);   
+    }
+}
---
+++ RedisOutputCacheProvider/RedisOutputCacheConnectionWrapper.cs
@@ -0,0 +1,90 @@
+﻿//
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+//
+
+using System;
+
+namespace Microsoft.Web.Redis
+{
+    internal class RedisOutputCacheConnectionWrapper : IOutputCacheConnection
+    {
+        internal static RedisSharedConnection sharedConnection;
+        static object lockForSharedConnection = new object();
+        internal static RedisUtility redisUtility;
+
+        internal IRedisClientConnection redisConnection;
+        ProviderConfiguration configuration;
+        
+        public RedisOutputCacheConnectionWrapper(ProviderConfiguration configuration)
+        {
+            this.configuration = configuration;
+            
+            // Shared connection is created by server when it starts. don't want to lock everytime when check == null.
+            // so that is why pool == null exists twice.
+            if (sharedConnection == null)
+            {
+                lock (lockForSharedConnection)
+                {
+                    if (sharedConnection == null)
+                    {
+                        sharedConnection = new RedisSharedConnection(configuration,() => new StackExchangeClientConnection(configuration));
+                        redisUtility = new RedisUtility(configuration);
+                    }
+                }
+            }
+            redisConnection = sharedConnection.TryGetConnection();
+        }
+
+/*-------Start of Add operation-----------------------------------------------------------------------------------------------------------------------------------------------*/
+        // KEYS = { key }
+        // ARGV = { page data, expiry time in miliseconds } 
+        // retArray = { page data from cache or new }
+        static readonly string addScript = (@"
+                    local retVal = redis.call('GET',KEYS[1])
+                    if retVal == false then
+                       redis.call('PSETEX',KEYS[1],ARGV[2],ARGV[1])
+                       retVal = ARGV[1]
+                    end
+                    return retVal
+                    ");
+
+        public object Add(string key, object entry, DateTime utcExpiry)
+        {
+            key = GetKeyForRedis(key);
+            TimeSpan expiryTime = utcExpiry - DateTime.UtcNow;
+            string[] keyArgs = new string[] { key };
+            object[] valueArgs = new object[] { redisUtility.GetBytesFromObject(entry), (long) expiryTime.TotalMilliseconds };
+
+            object rowDataFromRedis = redisConnection.Eval(addScript, keyArgs, valueArgs);
+            return redisUtility.GetObjectFromBytes(redisConnection.GetOutputCacheDataFromResult(rowDataFromRedis));
+        }
+
+/*-------End of Add operation-----------------------------------------------------------------------------------------------------------------------------------------------*/
+
+        public void Set(string key, object entry, DateTime utcExpiry)
+        {
+            key = GetKeyForRedis(key);
+            byte[] data = redisUtility.GetBytesFromObject(entry);
+            redisConnection.Set(key, data, utcExpiry);
+        }
+
+        public object Get(string key)
+        {
+            key = GetKeyForRedis(key);
+            byte[] data = redisConnection.Get(key);
+            return redisUtility.GetObjectFromBytes(data);
+        }
+
+        public void Remove(string key)
+        {
+            key = GetKeyForRedis(key);
+            redisConnection.Remove(key);
+        }
+
+        private string GetKeyForRedis(string key)
+        {
+            return configuration.ApplicationName + "_" + key;
+        }
+    }
+}
